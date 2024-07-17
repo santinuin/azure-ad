ff5fd3b2a993142bd57f819f66be4c190be1f8aa
Santiago Nuñez Ingas <snunez@chattigo.com>
2024-07-17 13:59:10 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'azure-ad';
+export class AppComponent implements OnInit {
+
+  title = 'Angular Azure AD Integration';
+  username: string = '';
+  password: string = '';
+
+
+  constructor(private authService: MsalService) { }
+
+  ngOnInit(): void {
+    this.authService.instance.handleRedirectPromise().then((res) => {
+      if (res !== null && res.account !== null) {
+        this.authService.instance.setActiveAccount(res.account);
+      }
+    });
+  }
+
+  login() {
+    this.authService.loginRedirect();
+  }
+
+  logout() {
+    this.authService.logoutRedirect();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { MsalModule, MsalService, MsalGuard, MsalBroadcastService, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,9 +11,30 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MsalModule.forRoot(new PublicClientApplication({
+      auth: {
+        clientId: 'YOUR_CLIENT_ID', // Sustituye por tu Client ID
+        authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID', // Sustituye por tu Tenant ID
+        redirectUri: 'http://localhost:4200' // URL de tu aplicación Angular
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        /*storeAuthStateInCookie: isIE,*/ // set to true for IE 11
+      }
+    }), {
+      interactionType: InteractionType.Redirect,
+      authRequest: {
+        scopes: ['user.read']
+      }
+    }, {
+      interactionType: InteractionType.Redirect,
+      protectedResourceMap: new Map([
+        ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+      ])
+    }),
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MsalService, MsalGuard, MsalBroadcastService],
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule { }
